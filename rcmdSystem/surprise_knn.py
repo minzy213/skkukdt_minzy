bdd05efcfa116c805b8d55c3ba5e48bfb3f37538
SKKU-02\User <olmmlp@naver.com>
2023-08-04 08:53:43 +0900

    [수업자료][추천시스템] Surprise를 활용한 KNN 유사도 계산해보기 ml dataset, KNN 모델들로 최적화 연습 - 모델을 바꿔가면서 test - 유사도 변경해가면서 test - 최적의 K값을 찾기

--- rcmdSystem/surprise_knn.py
+++ rcmdSystem/surprise_knn.py
@@ -1,139 +1,169 @@
 # %%
 import pandas as pd
-import matplotlib.pyplot as plt
-
-# %%
-# 파일 읽어오기
-rating_df = pd.read_csv('u.data', sep='\t', header=None, names = ['userID', 'movieID', 'rating', 'timestamp'])
-
-# %%
-# Reader = 읽기 위한 객체, 이걸로 rating_df를 dataset으로 읽어온다.
-from surprise import Dataset, Reader
-
-rdr = Reader(line_format="user item rating", sep="\t") ## 반드시 사용자-아이템-평점 순서로
-data = Dataset.load_from_df(rating_df[['userID', 'movieID', 'rating']], reader=rdr)
-
-# %%
-# split
-from surprise.model_selection import train_test_split
-trainset, testset = train_test_split(data, test_size=0.2, random_state=1004)
-testset
+import numpy as np
+from math import log
+from matplotlib import pyplot as plt
 
 # %% [markdown]
-# ## KNN으로 추천시스템 맛보기
-
-# %%
-# KNN 모델 불러와서 fit
-from surprise import KNNBasic
-recom_sys = KNNBasic()
-recom_sys.fit(trainset)
-
-# %%
-# testset의 예측 결과 보기
-# uid=741, iid=281, r_ui=2.0, est=3.0157043977599276
-# userid, itemid, actual, predict
-pred = recom_sys.test(testset)
-pred[:10]
-
-# %%
-# testset의 rmse error 확인하기
-from surprise import accuracy
-accuracy.rmse(recom_sys.test(testset))
-
-# %%
-# 예측 결과 예쁘게 출력해보기
-for p in pred[:20]:
-    print(f'user : {p.iid:<4}, movie : {p.uid}, rating : {p.r_ui} -- > {p.est:.2f}, diff : {p.r_ui - p.est:>5.2f}')
-
-# %%
-# error 그래프 그려보기
-error_list = []
-for p in pred:
-    error_list.append(p.r_ui - p.est)
-    
-plt.style.use('seaborn')
-plt.hist(error_list, bins = 20)
-# vertical line. 이 안에 들어오는 것들은 반올림하면 정확하게 예측된 것.
-plt.axvline(-0.5, color = 'red')
-plt.axvline(0.5, color = 'red')
-# 이 안에 들어오는 것들은 반올림하면 별점 1 틀린 것.
-plt.axvline(-1.5, color = 'orange')
-plt.axvline(1.5, color = 'orange')
+# ## Text content 기반 추천 시스템 순서
+# 1. 문장을 벡터화한다(TF-IDF)
+# 1. 모든 문장들의 유사도를 계산한다. (cosine)
+# 1. 나를 제외한 가장 유사한 문장들로 정렬한다.
 
 # %% [markdown]
-# 별점 예측은 지도 학습, 회귀 모델.
-
-# %% [markdown]
-# ### 랜덤으로 testdata 하나 만들어서 이사람한테 영화 추천해주기!
+# ### TF-IDF 실습
 # 
-# 1. 유저 선택   
-# 1. 유저가 안본 영화 리스트   
-# 1. 영화 리스트에 대한 평점 전부 예측   
-# 1. 예측 평점이 가장 높은 N개의 영화 선택
+# 문서 내 단어의 빈도수에 해당 단어가 등장한 문서 빈도의 역수를 함께 계산한 것.   
+# -> **다른 문서에서도 많이 등장한 단어면 가중치가 낮고, 다른 문서에서 많이 등장하지 않은 단어면 가중치가 높은 단어**.    
+# 
+# 빈도는 낮을지 몰라도 다른 문서에서 언급되지 않은 단어라면 그 단어는 해당 문서의 아이덴티티일 수 있다.
 
 # %%
-# 1. 유저 선택
-import random
-# 랜덤으로 testdata 하나 만들어서 사람 하나 뽑기
-usr, mv, rating = testset[random.randint(0, 20000)]
-usr, mv, rating
+docs = [
+  'I like a red apple',
+  'the color of the banana is yellow',
+  'long and yellow sweet banana',
+  'I like fruits. especially apple and banana'
+] 
+vocab = list(set(w for doc in docs for w in doc.split()))
+vocab.sort()
 
 # %%
-movie_info_df = pd.read_csv('u.item', sep='|', encoding='latin', header=None)
-movie_info_df.columns = ['movieID' , 'movie_title' , 'release_date' , 'video_release_date' ,
-                        'IMDb_URL' , 'unknown' , 'Action' , 'Adventure' , 'Animation' ,
-                        'Children' , 'Comedy' , 'Crime' , 'Documentary' , 'Drama' , 'Fantasy' ,
-                        'Film-Noir' , 'Horror' , 'Musical' , 'Mystery' , 'Romance' , 'Sci-Fi' ,
-                        'Thriller' , 'War' , 'Western']
+N = len(docs) 
 
-movie_name = movie_info_df.loc[mv]
-print(movie_name['movie_title'], '- predicted score : ', pred.est, 'real score :', rating)
-print(f'{movie_name.movie_title} - predicted score : , {pred.est:.2f}, real score : {rating}')
+def tf(t, d):
+    words_set = d.split(' ')
+    return words_set.count(t)
 
-# %%
-# 2. 유저가 안본 영화 리스트
+def idf(t):
+    df = 0
+    for doc in docs:
+        df += t in doc
+    return log(N/(df + 1))
 
-# 전체 영화 id 가져옴
-all_movie_ids = rating_df.movieID.unique()
-# 이 사람이 본 영화 id 가져옴
-watched_movie_ids = rating_df[rating_df['userID']==usr].movieID
-# 전체 영화에서 이 사람이 본 영화 id 삭제
-target_mv_list = set(all_movie_ids) - set(watched_movie_ids)
+def tfidf(t, d):
+    return tf(t,d)* idf(t)
 
 # %%
-# 영화 리스트에 대한 평점 전부 예측, 
-# 하나의 유저에 대해 모든 target mv list의 예상 점수 dict
-# dataframe으로 바로 변환하려면 dictionary를 list로 저장!
-all_pred_scores = []
-for s_mv_id in target_mv_list:
-    
-    all_pred_scores.append({'movieID' : s_mv_id, 'pred_score':recom_sys.predict(usr, s_mv_id).est})
+# TF(Term Frequency) table : 빈도
+result = []
+for i in range(N): 
+    result.append([])
+    d = docs[i]
+    for j in range(len(vocab)):
+        t = vocab[j]        
+        result[-1].append(tf(t, d))
+tf_df = pd.DataFrame(result, columns = vocab)
 
-pred_df = pd.DataFrame(all_pred_scores)
-pred_df
+# IDF(Inverse Document Frequency) Table : 희소성
+result = []
+for j in range(len(vocab)):
+    t = vocab[j]
+    result.append(idf(t))
+idf_df = pd.DataFrame(result, index=vocab, columns=["IDF"])
 
-# %%
-# 예측 평점이 가장 높은 N개의 영화 선택
-pred_df = pred_df.sort_values('pred_score', ascending=False)
-recom_mv_ids = pred_df[pred_df['pred_score'] == 5]
+# TF-IDF 테이블(벡터화) 구하기
+result = []
+for i in range(N):
+  result.append([])
+  d = docs[i]
+  for j in range(len(vocab)):
+    t = vocab[j]
+    result[-1].append(tfidf(t,d))
+tfidf_df = pd.DataFrame(result, columns = vocab)
+tfidf_df
 
-# %%
-result_df = pd.merge(movie_info_df, recom_mv_ids, on='movieID')
-result_df['movie_title']
 
 # %% [markdown]
-# ### 많은 모델의 예측 성능 보기
+# #### sklearn님의 힘을 빌려서 동일한 작업 후 similarity 구하기
 
 # %%
-from surprise import KNNBasic, BaselineOnly, CoClustering, KNNBaseline, NMF, SVD
-models = [KNNBasic(), BaselineOnly(), CoClustering(random_state=0), KNNBaseline(), NMF(random_state=0), SVD(random_state=0)]
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-for m in models:
-    m.fit(trainset)
-    accuracy.rmse(m.test(testset))
+docs = [
+  'I like a red apple',
+  'the color of the banana is yellow',
+  'long and yellow sweet banana',
+  'I like fruit especially apples and bananas'
+] 
 
+# 4개 문장 벡터화
+tfidfv = TfidfVectorizer().fit(docs)
+rfidf_arr = tfidfv.transform(docs).toarray()
+rfidf_arr
 
 # %%
+from sklearn.metrics.pairwise import cosine_similarity
 
+# 위에서 벡터화 한 4개 문장 행렬의 다른 문장들과의 similarity 계산.
+cos_matrix = cosine_similarity(rfidf_arr, rfidf_arr)
+plt.imshow(cos_matrix)
+plt.colorbar()
+plt.show()
+cos_matrix
+
+# %%
+# 1번 인덱스 문장과 가장 유사한 문장 순서대로 index 출력하기
+my_index = 1
+sim_sorted_idx_arr = np.argsort(cos_matrix[my_index], axis=0)[::-1][1:]
+sim_sorted_idx_arr
+
+# %% [markdown]
+# ### kaggle movie 데이터로 text content 기반 추천 시스템 해보기
+# 영화의 설명이 비슷한 영화 추천
+# 
+# #### 1. 문장을 벡터화한다(TF-IDF)
+
+# %%
+m_df = pd.read_csv('..\dataset\movies_metadata.csv', low_memory = False)[['id', 'title', 'overview']]
+m_df.head()
+
+# %%
+m_df.isna().sum()
+
+# %%
+m_df = m_df.dropna()
+
+print(len(m_df))
+## 중복 제거!!! 항상 조심하자
+m_df = m_df.drop_duplicates()
+print(len(m_df))
+
+# 제목이랑 오버뷰도 중복인게 있다...
+# 비어 있거나 No overview 이런거 있을수도 있음. 제거하고 해야 한다.
+print(m_df.nunique())
+
+# %%
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# max_features : 빈도 높은 3만개만 남기고 나머지는 버림
+tfidf = TfidfVectorizer(max_features = 30000)
+ov_vec = tfidf.fit_transform(m_df['overview'])
+# ov_vec은 sparse matrix, 지금은 쓸 줄 모르니까 array로 바꿔서 사용
+# toarray하면 짱느리다... 일단 빼고 하자
+# ov_vec = ov_vec.toarray()
+ov_vec.shape
+
+# %% [markdown]
+# #### 2. 모든 문장들의 유사도를 계산한다. (cosine)
+
+# %%
+from sklearn.metrics.pairwise import cosine_similarity
+cosine_sim = cosine_similarity(ov_vec, ov_vec)
+
+cos_sim_df = pd.DataFrame(cosine_sim, index = m_df.title , columns=m_df.title)
+cos_sim_df
+
+# %% [markdown]
+# #### 3. 나를 제외한 가장 유사한 문장들로 정렬한다.
+
+# %%
+# Toy Story와 설명이 가장 유사한 영화 제목을 보고 싶어 !
+m_name = 'Toy Story'
+cos_sim_df[m_name].sort_values(ascending = False).head(11)
+
+# %%
+cos_sim_df['Toy Story']
 
 
